738071dae49d1238b68b1b4168633d45dd944020
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-08-03 06:19:15 +0100

    update

--- server/index.js
+++ server/index.js
@@ -26,15 +26,7 @@ const UsersState = {
 };
 
 const io = new Server(expressServer, {
-  cors: {
-    origin:
-      process.env.NODE_ENV === "production"
-        ? ["https://stephen-websocket-chatapp.vercel.app"]
-        : ["http://localhost:5500", "http://127.0.0.1:5500"],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true,
-  },
+  cors: "*",
 });
 
 io.on("connection", (socket) => {
